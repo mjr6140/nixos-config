cc11469982fb55cef21ad01228adfde5d0fbaab5
Matt Rickard <mjr6140@gmail.com>
2025-12-21 18:59:47 -0500

    feat: Enable Flatpak and add Flathub remote.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -30,4 +30,14 @@
   # Gaming (shared across all desktop-like hosts)
   programs.steam.enable = true;
   programs.gamemode.enable = true;
+
+  # Flatpak & Flathub
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }

1b9d077d1a9c1dadd17fcad26ba1f3b60f00d367
Matt Rickard <mjr6140@gmail.com>
2025-12-21 19:33:28 -0500

    feat: Configure detailed file system mounts for root, home, nix, log, boot, and storage using UUIDs and Btrfs subvolumes.

--- hosts/nixos-desktop/hardware-configuration.nix
+++ hosts/nixos-desktop/hardware-configuration.nix
@@ -11,9 +11,46 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos"; # Placeholder
+    { device = "/dev/disk/by-uuid/<uuid>";
       fsType = "btrfs";
+      options = [ "subvol=@" "compress=zstd" "noatime" "discard=async"];
     };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/<uuid>";
+      fsType = "btrfs";
+      options = [ "subvol=@home" "compress=zstd" "noatime" "discard=async"];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/<uuid>";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" "compress=zstd" "noatime" "discard=async"];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/<uuid>";
+      fsType = "btrfs";
+      options = [ "subvol=@log" "compress=zstd" "noatime" "discard=async"];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/<uuid-efs-boot>";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  # Storage mount for photos disk
+  fileSystems."/mnt/storage" = {
+    device = "/dev/disk/by-uuid/51BAE4A7240C12DE";
+    fsType = "auto";
+    options = [
+      "nosuid"
+      "nodev"
+      "nofail"
+      "x-gvfs-show"
+    ];
+  };
 
   swapDevices = [ ];
 
